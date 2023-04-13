87b61872b6def297e83cbba91de20dfc9b5df64c
Igor Avdanin <wednesday@bk.ru>
2023-04-14 00:43:06 +0300

    4  задача

--- Program.cs
+++ Program.cs
@@ -64,3 +64,20 @@
 //     Console.WriteLine("Число является нечетным");   
 // }
 
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int n=Convert.ToInt32(Console.ReadLine());
+int nn=1;
+int result = nn%2;
+while (n > nn)
+{
+    if ( result == 0)
+{
+Console.WriteLine(nn);  
+}
+else
+{
+ nn = nn + 1;
+} 
+}
+//не понял как делать
